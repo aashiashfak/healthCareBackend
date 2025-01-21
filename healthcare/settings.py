a0cae0a7fb8796ffc8e9a1676ea44e50ade6c251
Aashi Ashfak <150011814+aashiashfak@users.noreply.github.com>
2025-01-21 10:12:50 +0530

    updated allowed host

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -32,11 +32,13 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "13.60.111.177",
     "health-carefrontend-git-main-aashis-projects-14744245.vercel.app",
+    "health-care-backend-ivory.vercel.app",
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://127.0.0.1:5173",
     "https://health-carefrontend-git-main-aashis-projects-14744245.vercel.app",  
+    "https://health-care-backend-ivory.vercel.app/",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 # CORS_ALLOW_HEADERS = [
